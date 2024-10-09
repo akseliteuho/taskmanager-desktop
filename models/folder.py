4a80c140acac7e92c667b4a863786babbf3abdd5
akseliteuho <ajteuh@utu.fi>
2024-10-09 18:28:55 +0300

    Changes

--- models/folder.py
+++ models/folder.py
@@ -1,12 +1,11 @@
 from .database import Database
-from .sql_database import SQLDatabase
 
 # Folder luokan avulla hallitaan/käsitellään kansioita.
 class Folder:
     # Konstruktori, joka saa parametrinaan kansion nimen
     def __init__(self, name):
-        self.db = SQLDatabase()
-        self.name = name
+        self.db = Database() # Luodaan tietokanta yhteys
+        self.name = name 
 
     # Tallennetaan kansio tietokantaan lisäämällä kansion nimi tietokantaan
     def save(self):
@@ -16,20 +15,20 @@ class Folder:
     # Haetaan kaikki kansiot tietokannasta
     @staticmethod
     def get_all_folders():
-        db = SQLDatabase() # Luodaan tietokanta yhteys luomalla Database luokan instanssi
+        db = Database() # Luodaan tietokanta yhteys luomalla Database luokan instanssi
         cursor = db.conn.execute("SELECT * FROM folders") # Haetaan kaikki kansiot tietokannasta SQL kyselyllä
         return cursor.fetchall() # Palautetaan kaikki kansiot listana
     
     @staticmethod
     def get_folder_by_id(folder_id):
-        db = SQLDatabase() # Luodaan tietokanta yhteys luomalla Database luokan instanssi
+        db = Database() # Luodaan tietokanta yhteys luomalla Database luokan instanssi
         cursor = db.conn.execute("SELECT * FROM folders WHERE id = ?", (folder_id,)) # Haetaan kansio kansion ID:n perusteella SQL kyselyllä
         return cursor.fetchone() # Palautetaan kansio
 
     # Poistetaan kansio tietokannasta kansion ID:n perusteella
     @staticmethod
     def delete_folder(folder_id): 
-        db = SQLDatabase() # Luodaan tietokanta yhteys
+        db = Database() # Luodaan tietokanta yhteys
         with db.conn: # Avataan tietokanta yhteys
             db.conn.execute("DELETE FROM folders WHERE id = ?", (folder_id,)) # Poistetaan kansio tietokannasta SQL kyselyllä
 
--- models/task.py
+++ models/task.py
@@ -1,11 +1,10 @@
 from .database import Database
-from .sql_database import SQLDatabase
 
 # Task luokka, jolla hallitaan/käsitellään tehtäviä
 class Task:
     # Konstruktori, joka initialisoi Task luokan ominaisuudet
     def __init__(self, title, description='', due_date=None, folder_id=None):
-        self.db = SQLDatabase() # Luodaan tietokanta yhteys
+        self.db = Database() # Luodaan tietokanta yhteys
         self.title = title # Tehtävän otsikko
         self.description = description # Tehtävän kuvaus
         self.due_date = due_date # Tehtävän deadline
@@ -24,20 +23,20 @@ class Task:
     # Haetaan kaikki tehtävät tietokannasta ja palautetaan ne listana
     @staticmethod
     def get_all_tasks():
-        db = SQLDatabase() # Luodaan tiedokanta yhteys luomalla Database luokan instanssi
+        db = Database() # Luodaan tiedokanta yhteys luomalla Database luokan instanssi
         cursor = db.conn.execute("SELECT * FROM tasks") # Haetaan kaikki tehtävät tietokannasta SQL kyselyllä
         return cursor.fetchall() # Palautetaan kaikki tehtävät listana
 
     # Haetaan tehtävät kansion ID:n perusteella
     @staticmethod
     def get_tasks_by_folder_id(folder_id):
-        db = SQLDatabase() # Luodaan tiedokanta yhteys luomalla Database luokan instanssi
+        db = Database() # Luodaan tiedokanta yhteys luomalla Database luokan instanssi
         cursor = db.conn.execute("SELECT * FROM tasks WHERE folder_id = ?", (folder_id,)) # Haetaan tehtävät kansion ID:n perusteella SQL kyselyllä
         return cursor.fetchall() # Palautetaan tehtävät listana
 
     # Poistetaan tehtävä tietokannasta tehtävän ID:n perusteella
     @staticmethod
     def delete_task(task_id):
-        db = SQLDatabase() # Luodaan tiedokanta yhteys luomalla Database luokan instanssi
+        db = Database() # Luodaan tiedokanta yhteys luomalla Database luokan instanssi
         with db.conn: # Avataan tietokanta yhteys
             db.conn.execute("DELETE FROM tasks WHERE id = ?", (task_id,)) # Poistetaan tehtävä tietokannasta SQL kyselyllä
