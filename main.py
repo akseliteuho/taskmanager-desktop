52a80c8f2f7f62c3634abc1a3dddfe4544fe4a91
akseliteuho <ajteuh@utu.fi>
2024-10-16 11:29:00 +0300

    Additions

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 from views.MainView import MainView
 
-# MainView luokka on vastuussa käyttöliittymän alustamisesta ja hallitsemisesta.
+# Käynnistetään sovellus
 if __name__ == "__main__":
-    root = tk.Tk() # Luodaan pääikkuna
+    root = tk.Tk() # Luodaan pääikkuna käyttöliittymälle
     app = MainView(root) # Luodaan MainView luokan instanssi, jotta GUI saadaan näkyviin
     root.mainloop() # Käynnistetään pääikkuna
